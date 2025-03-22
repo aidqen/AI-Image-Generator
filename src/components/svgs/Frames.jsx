516795771c7b8144ec6b929c8c35af74c21293aa
aidqen <idanmarkin8@gmail.com>
2025-03-22 21:09:38 +0200

    Add SVGs

---
+++ src/components/svgs/Frames.jsx
@@ -0,0 +1,11 @@
+export function Frames() {
+
+    return <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="9" height="9" rx="2" fill="#E4E4E7"/>
+    <rect x="11" width="9" height="9" rx="2" fill="#E4E4E7" fillOpacity="0.2"/>
+    <rect y="11" width="9" height="9" rx="2" fill="#E4E4E7" fillOpacity="0.2"/>
+    <rect x="11" y="11" width="9" height="9" rx="2" fill="#E4E4E7" fillOpacity="0.2"/>
+    </svg>
+    
+
+}
